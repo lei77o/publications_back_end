2b2846e3b0da9ac71f7157d3f27cfe9eb75e292e
Leonardo Albrecht <lalbrecht@fxone.com>
2023-07-18 16:32:43 -0300

    fixed problem with local variables

--- src/db.js
+++ src/db.js
@@ -8,11 +8,3 @@ export const pool = createPool({
     port: DB_PORT,
     database: DB_DATABASE,
 })
-
-console.log({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_DATABASE,
-});
