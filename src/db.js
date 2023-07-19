2f0494bb136615dfff1dc2eff6631f957c3e5a49
Leonardo Albrecht <lalbrecht@fxone.com>
2023-07-18 23:14:38 -0300

    variable names were changed

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import { createPool } from "mysql2/promise";
-import {DB_HOST, DB_DATABASE, DB_PASSWORD, DB_USER, DB_PORT} from './config.js'
+import {MYSQLHOST, MYSQLDATABASE, MYSQLPASSWORD, MYSQLUSER, MYSQLPORT} from './config.js'
 
 export const pool = createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_DATABASE,
+    host: MYSQLHOST,
+    user: MYSQLUSER,
+    password:MYSQLPASSWORD,
+    port: MYSQLPORT,
+    database: MYSQLDATABASE,
 })
